45e43032f6afb891f4042041eff63b9e9984ceb9
angel zambrano <angeldzambrano99@gmail.com>
2022-10-08 20:44:41 -0700

    Add files via upload

---
+++ src/chat/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, re_path
+from .views import index
+from .views import room
+
+
+app_name='chat'
+urlpatterns = [
+    path('', index, name='index'),
+    path('<str:room_name>/', room, name='room'),
+
+
+
+]
